284dad892c578edb286187b65f9706eeeba6a509
matkv <mail@matkv.dev>
2024-06-13 20:57:06 +0200

    first ugly version of successful login to Spotify

--- PlaylistQuickAdd/Authorization.cs
+++ PlaylistQuickAdd/Authorization.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Web;
 using Windows.System;
 
 namespace PlaylistQuickAdd
@@ -43,16 +45,50 @@ namespace PlaylistQuickAdd
 
         public async Task Login()
         {
+            var redirectUri = "http://localhost:3000"; 
+            var state = Guid.NewGuid().ToString();
+
+
             using var client = new HttpClient();
 
             var clientId = Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_ID");
-            var redirectUri = "http://localhost:3000"; 
-            var state = Guid.NewGuid().ToString();
             var scope = "user-read-private user-read-email";
 
             var response = await client.GetAsync($"https://accounts.spotify.com/authorize?client_id={clientId}&response_type=code&redirect_uri={redirectUri}&scope={scope}&state={state}");
 
             await Launcher.LaunchUriAsync(new Uri(response.RequestMessage.RequestUri.ToString()));
+            await StartHTTPListenerAsync(redirectUri, state);
+
+            var test = response.Content.ReadAsStringAsync();
+        }
+
+        private async Task StartHTTPListenerAsync(string redirectUri, string state)
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add(redirectUri + "/");
+            listener.Start();
+
+            var context = await listener.GetContextAsync(); // This will wait until a request is received
+
+            var response = context.Response;
+            string responseString = "<html><body>Please return to the app.</body></html>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
+
+            var queryParams = HttpUtility.ParseQueryString(context.Request.Url.Query);
+            var code = queryParams["code"];
+            var stateReceived = queryParams["state"];
+
+            // Ensure state matches to prevent CSRF
+            if (stateReceived == state)
+            {
+                // Process the authorization code as needed
+                Console.WriteLine($"Authorization code: {code}");
+            }
+
+            listener.Stop();
         }
     }
 
