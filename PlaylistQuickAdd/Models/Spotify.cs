0de7ca4ac4544e913bd155aab8047192a7525894
matkv <mail@matkv.dev>
2024-07-08 20:38:22 +0200

    started with main business logic class but I'm tired

---
+++ PlaylistQuickAdd/Models/Spotify.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistQuickAdd.Models
+{
+    public class Spotify
+    {
+        // TODO this class is supposed to handle all of the background logic with the Spotify API
+
+        public SpotifyUser User { get; set; }
+
+        public Spotify()
+        {
+            
+        }
+    }
+}
