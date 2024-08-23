7113b7bf59bd483025378b793c19e529d0097885
matkv <mail@matkv.dev>
2024-08-23 20:22:47 +0200

    set up QuickAdd viewmodel

---
+++ PlaylistQuickAdd/ViewModels/QuickAddViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using PlaylistQuickAdd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistQuickAdd.ViewModels
+{
+    internal class QuickAddViewModel
+    {
+        public SharedDataService sharedDataService;
+
+        public QuickAddViewModel()
+        {
+            SetupSharedDataService();
+        }
+
+        public void SetupSharedDataService()
+        {
+            var app = (App)Application.Current;
+
+            var serviceProvider = app.ServiceProvider;
+            sharedDataService = serviceProvider.GetService<SharedDataService>();
+        }
+    }
+}
