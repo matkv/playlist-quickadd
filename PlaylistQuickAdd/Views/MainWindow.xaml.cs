2ccb0ccd401168c5aabbb9d349b602ddc40e48aa
matkv <mail@matkv.dev>
2024-10-28 20:23:11 +0100

    fix namespace for main window

--- PlaylistQuickAdd/Views/MainWindow.xaml.cs
+++ PlaylistQuickAdd/Views/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using PlaylistQuickAdd.Views;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace PlaylistQuickAdd;
+namespace PlaylistQuickAdd.Views;
 
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
